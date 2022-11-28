a182b64e7145ec7d9975247dfb50676b3695b350
Junghun Sim <96688359+092600@users.noreply.github.com>
2022-11-28 22:14:09 +0900

    2022 11 28 / Nodam_public

---
+++ nodam_public/src/main/java/com/nodam/nodam_public/NodamPublicApplication.java
@@ -0,0 +1,13 @@
+package com.nodam.nodam_public;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodamPublicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodamPublicApplication.class, args);
+    }
+
+}
---
+++ nodam_public/src/test/java/com/nodam/nodam_public/NodamPublicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nodam.nodam_public;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodamPublicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
